77e170935ed869938f7ebda5caf3adde0085c25e
Pobiega <robin.aakerman@gmail.com>
2019-05-29 16:41:42 +0200

    Adhere to javascript formating

--- bit.js
+++ bit.js
@@ -1,33 +1,32 @@
 class Bit {
-  constructor(x, y, diameter) {
-    this.x = x;
-    this.y = y;
-    this.diameter = diameter;
-    this.state = false;
-  }
-  
-  draw() {
-    stroke(255);
-    if(this.state)
-      fill(0,155,0);
-    else
-      fill(255,0,0);
-    ellipse(this.x, this.y, this.diameter);
-  }
-  
-  setState(state) {
-    this.state = Boolean(parseInt(state));
-  }
-  
-  contains(x,y) {
-    let d = dist(x,y,this.x, this.y);
-    if (d < this.diameter/2)
-    {
-      this.toggle();
+    constructor(x, y, diameter) {
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.state = false;
     }
-  }
-  
-  toggle() {
-    this.state = !this.state;
-  }
+
+    draw() {
+        stroke(255);
+        if (this.state)
+            fill(0, 155, 0);
+        else
+            fill(255, 0, 0);
+        ellipse(this.x, this.y, this.diameter);
+    }
+
+    setState(state) {
+        this.state = Boolean(parseInt(state));
+    }
+
+    contains(x, y) {
+        let d = dist(x, y, this.x, this.y);
+        if (d < this.diameter / 2) {
+            this.toggle();
+        }
+    }
+
+    toggle() {
+        this.state = !this.state;
+    }
 }
--- sketch.js
+++ sketch.js
@@ -3,47 +3,46 @@ let byte = [];
 let decimalP;
 
 function mousePressed() {
-  for (let i = 0; i < byte.length; i++) {
-    byte[i].contains(mouseX, mouseY);
-  }
+    for (let i = 0; i < byte.length; i++) {
+        byte[i].contains(mouseX, mouseY);
+    }
 }
 
 function setup() {
-  const size = initialState.length;
-  createCanvas((size/2)*100, 100);
-  decimalP = createDiv();
+    const size = initialState.length;
+    createCanvas((size / 2) * 100, 100);
+    decimalP = createDiv();
 
-  let w = width / size;
-  for (let i = 0; i < size; i++) {
-    byte[i] = new Bit(w / 2 + i * w, 50, w - 4);
-    byte[i].setState(initialState.charAt(i));
-  }
+    let w = width / size;
+    for (let i = 0; i < size; i++) {
+        byte[i] = new Bit(w / 2 + i * w, 50, w - 4);
+        byte[i].setState(initialState.charAt(i));
+    }
 }
 
 function draw() {
-  background(220);
+    background(220);
 
-  for (let i = 0; i < byte.length; i++) {
-    byte[i].draw();
-  }
+    for (let i = 0; i < byte.length; i++) {
+        byte[i].draw();
+    }
 
-  decimalP.html(binaryToDecimal(byteToBinary(byte)));
+    decimalP.html(binaryToDecimal(byteToBinary(byte)));
 }
 
-function byteToBinary(byte)
-{
-  let binary = '';
-  for (let i = 0; i < byte.length; i++) {
-    binary += byte[i].state ? '1' : '0';
-  }
-  return binary;
+function byteToBinary(byte) {
+    let binary = '';
+    for (let i = 0; i < byte.length; i++) {
+        binary += byte[i].state ? '1' : '0';
+    }
+    return binary;
 }
 
 function binaryToDecimal(val) {
-  let sum = 0;
-  for (let i = 0; i < val.length; i++) {
-    let bit = val.charAt(val.length - i - 1);
-    sum += parseInt(bit) * pow(2, i);
-  }
-  return sum;
+    let sum = 0;
+    for (let i = 0; i < val.length; i++) {
+        let bit = val.charAt(val.length - i - 1);
+        sum += parseInt(bit) * pow(2, i);
+    }
+    return sum;
 }
